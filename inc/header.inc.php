056a5dcb214e8b759f85777836b46ca8e58c98d9
BloomfieldK <32702938+BloomfieldK@users.noreply.github.com>
2018-05-29 20:18:40 +0300

    Update header.inc.php

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -3,11 +3,22 @@
 <head>
       <meta charset="UTF-8">
       <title>GestImmo</title>
-      <link rel="stylesheet" type="text/css" media="screen" href="../asset/style.css">
-      <link rel="stylesheet" type="text/css" media="print" href="../asset.impression.css">
+     
       <script language=javascript src="../asset/mesfonctions.js"></script>
+      <style>background-color: #BCCE98</style>
+
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
    </head>
-   <body>
+      <body style="background-color:;">
+            
+            
+   <!--
+   <link rel="stylesheet" type="text/css" media="screen" href="../asset/style.css">
+      <link rel="stylesheet" type="text/css" media="print" href="../asset.impression.css">
+<body>
       <div id="entete">
          <a href="../index.php"><img class="logo" src="../images/logo.png" alt="logo"></a>
          <h1  font-family:'Arial' font-size='48pt'>Gestion Agence Immobilière</h1>
